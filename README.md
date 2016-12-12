1f3380bb15894a7fef3f8f497f7cb7de5d07d404
Salim Salmi <salmi0707@gmail.com>
2016-12-12 17:29:31 +0100

    simple live reload setup and implemented world map and city locations

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# InstagramColorMap
-Visualization of instagram photos in large cities.
+# instagram-vis - Instagram
+
+## Running the project
+
+#### Install the node and bower dependencies
+```
+npm install
+bower install
+```
+
+#### Run gulp to
+Compile all files and start a webserver on [localhost:8000](http://localhost:8000):
+```
+gulp
+```
---
+++ app/src/index.js
@@ -0,0 +1,50 @@
+var width = 1440,
+    height = 810;
+
+var cities = { tokyo: { Longitude: 139.797171584, Latitude: 35.71781637 },
+  london: { Longitude: -0.097197167, Latitude: 51.532795 },
+  newyork: { Longitude: -74.04198026, Latitude: 40.72831085 },
+  berlin: { Longitude: 13.373883333, Latitude: 52.526611667 },
+  seoul: { Longitude: 127.012843219, Latitude: 37.565160341 },
+  moscow: { Longitude: 37.675085361, Latitude: 55.753156237 },
+  paris: { Longitude: 2.334376506, Latitude: 48.861800657 },
+  rome: { Longitude: 12.469875229, Latitude: 41.890391589 },
+  tehran: { Longitude: 51.3728388, Latitude: 35.6824128 },
+  stockholm: { Longitude: 18.038223494, Latitude: 59.342382941 } }
+
+var projection = d3.geo.mercator()
+    .scale(250)
+    .translate([width / 2, height / 1.5])
+    .rotate([0,0]);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var g = svg.append("g");
+
+
+// load and display the World map
+d3.json("../data/world-110m2.json", function(error, topology) {
+    g.selectAll("path")
+      .data(topojson.feature(topology, topology.objects.countries)
+          .features)
+    .enter()
+      .append("path")
+      .attr("d", path)
+
+    // mark cities on map
+    var locations = [];
+    for(var city in cities)
+      locations.push([cities[city].Longitude, cities[city].Latitude])
+    svg.selectAll("circle")
+  		.data(locations).enter()
+  		.append("circle")
+  		.attr("cx", function (d) { console.log(projection(d)); return projection(d)[0]; })
+  		.attr("cy", function (d) { return projection(d)[1]; })
+  		.attr("r", "8px")
+  		.attr("fill", "red")
+});
